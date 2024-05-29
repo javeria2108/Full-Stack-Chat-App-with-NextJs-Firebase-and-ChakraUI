df15ffa487e1ff8d9d40236b2f8bdac5adfd36b1
javeria2108 <javeriazh@gmail.com>
2024-05-29 20:30:55 +0500

    separate components for topbar and bottom bar

--- src/app/chat/[id]/page.js
+++ src/app/chat/[id]/page.js
@@ -1,35 +1,23 @@
 'use client'
 import Sidebar from "@/app/components/Sidebar";
-import { Avatar, Button, Flex, FormControl, Heading, Input,Text } from "@chakra-ui/react";
+import {  Flex,Text } from "@chakra-ui/react";
 import { collection, orderBy,query,doc } from "firebase/firestore";
-import { useRouter,useParams} from "next/navigation";
+import { useParams} from "next/navigation";
 import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
 import { db } from "../../../../firebaseconfig";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../../firebaseconfig";
 import getOtherEmail from "@/app/utils/getOtherEmail";
-const Topbar=({email})=>{
-    return(
-        <Flex bg='gray.100' h='81px' w='100%' align='center' p={5}>
-            <Avatar src="" marginEnd={3}/>
-            <Heading size='lg'>{email}</Heading>
-        </Flex>
-    )
-}
-const Bottombar=()=>{
-    return(
-      <FormControl p={3}>
-        <Input placeholder="type a message..."></Input>
-        <Button type="submit" hidden autoComplete='off'>submit</Button>
-      </FormControl>
-    )
-}
+import Topbar from "@/app/components/topbar";
+import Bottombar from "@/app/components/BottomBar";
+import { useEffect, useRef } from "react";
 export default function Chat(){
     const [user]=useAuthState(auth);
     const {id}=useParams();
     const q=query(collection(db,'chats',id,'messages'),orderBy('timestamp'))
     const [messages] = useCollectionData(q);
   const [chat]=useDocumentData(doc(db,"chats",id));
+  const BottomofChat=useRef();
     const getMessages = () => {
         return messages?.map((msg, index) => {
             const sender = msg.sender === user.email;
@@ -49,6 +37,15 @@ export default function Chat(){
             )
         });
     };
+
+    useEffect(()=>{
+        setTimeout(
+            BottomofChat.current.scrollIntoView({
+                behavior: 'smooth',
+                block:'start',
+            }),100
+        )
+    },[messages])
     return (
         <Flex h='100vh'>
         <Sidebar/>
@@ -57,8 +54,9 @@ export default function Chat(){
             <Flex flex={1} direction='column' pt={4} mx={5} overflowX='scroll' sx={{scrollbarWidth: 'none'}}>
 
                 {getMessages()}
+                <div ref={BottomofChat}></div>
             </Flex>
-            <Bottombar/>
+            <Bottombar id={id} user={user}/>
         </Flex>
         </Flex>
     )
---
+++ src/app/components/topbar.js
@@ -0,0 +1,10 @@
+import { Flex, Avatar,Heading } from "@chakra-ui/react";
+const Topbar=({email})=>{
+    return(
+        <Flex bg='gray.100' h='81px' w='100%' align='center' p={5}>
+            <Avatar src="" marginEnd={3}/>
+            <Heading size='lg'>{email}</Heading>
+        </Flex>
+    )
+}
+export default Topbar;
