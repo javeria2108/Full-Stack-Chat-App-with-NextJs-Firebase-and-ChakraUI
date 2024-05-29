0e38513932a9ae111cbbd9334ed0bfdf3d4ed23a
javeria2108 <javeriazh@gmail.com>
2024-05-29 19:21:32 +0500

    handling chat msgs from firestore

--- src/app/chat/[id]/page.js
+++ src/app/chat/[id]/page.js
@@ -1,15 +1,18 @@
 'use client'
 import Sidebar from "@/app/components/Sidebar";
-import { Avatar, Button, Flex, FormControl, Heading, Input } from "@chakra-ui/react";
-import { collection, orderBy,query } from "firebase/firestore";
+import { Avatar, Button, Flex, FormControl, Heading, Input,Text } from "@chakra-ui/react";
+import { collection, orderBy,query,doc } from "firebase/firestore";
 import { useRouter,useParams} from "next/navigation";
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
 import { db } from "../../../../firebaseconfig";
-const Topbar=()=>{
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../../firebaseconfig";
+import getOtherEmail from "@/app/utils/getOtherEmail";
+const Topbar=({email})=>{
     return(
         <Flex bg='gray.100' h='81px' w='100%' align='center' p={5}>
             <Avatar src="" marginEnd={3}/>
-            <Heading size='lg'>user@gmail.com</Heading>
+            <Heading size='lg'>{email}</Heading>
         </Flex>
     )
 }
@@ -22,64 +25,38 @@ const Bottombar=()=>{
     )
 }
 export default function Chat(){
-    const router=useRouter();
+    const [user]=useAuthState(auth);
     const {id}=useParams();
     const q=query(collection(db,'chats',id,'messages'),orderBy('timestamp'))
     const [messages] = useCollectionData(q);
-    console.log(messages);
+  const [chat]=useDocumentData(doc(db,"chats",id));
+    const getMessages = () => {
+        return messages?.map((msg, index) => {
+            const sender = msg.sender === user.email;
+            return (
+                <Flex
+                    key={index}
+                    alignSelf={sender ? 'flex-start' : 'flex-end'}
+                    bg={sender ? 'blue.100' : "green.100"}
+                    w='fit-content'
+                    minWidth='100px'
+                    m={1}
+                    p={3}
+                    borderRadius="lg"
+                >
+                    <Text>{msg.text}</Text>
+                </Flex>
+            )
+        });
+    };
     return (
         <Flex h='100vh'>
         <Sidebar/>
         <Flex flex={1} direction='column'>
-            <Topbar/>
+            <Topbar email={getOtherEmail(chat?.users,user)}/>
             <Flex flex={1} direction='column' pt={4} mx={5} overflowX='scroll' sx={{scrollbarWidth: 'none'}}>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
-                <Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex><Flex bg='blue.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg">
-                    Ho, this is a dummy message
-                </Flex>
-                <Flex bg='green.100' w='fit-content' minWidth='100px' m={1} p={3} borderRadius="lg" alignSelf='flex-end'>
-                    copy that
-                </Flex>
+
+                {getMessages()}
             </Flex>
             <Bottombar/>
         </Flex>
